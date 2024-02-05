4ec35f19c9b6758a72d9aec6b8454e11c6ab31d6
linearworld <39855658+linearworld@users.noreply.github.com>
2024-02-05 22:42:20 +0800

    Update aging_fig3c.m

--- empirical_data_code_fig2&3/aging_fig3c.m
+++ empirical_data_code_fig2&3/aging_fig3c.m
@@ -1,16 +1,16 @@
 ind1=class_70==1;
 ind2=class_70==2;
 
-coefficient=polyfit(-A12(ind1),E1(ind1),1);  %用一次函数拟合曲线，想用几次函数拟合，就把n设成那个数
+coefficient=polyfit(-A12(ind1),E1(ind1),1);  
 y1=polyval(coefficient,-A12(ind1));
 plot(-A12(ind1),E1(ind1),'bo',-A12(ind1),y1,'b-')
 shuchu1=[-A12(ind1),y1];
 hold on
-coefficient=polyfit(-A12(ind2),E1(ind2),1);  %用一次函数拟合曲线，想用几次函数拟合，就把n设成那个数
+coefficient=polyfit(-A12(ind2),E1(ind2),1); 
 y1=polyval(coefficient,-A12(ind2));
 plot(-A12(ind2),E1(ind2),'ro',-A12(ind2),y1,'r-')
 shuchu2=[-A12(ind2),y1];
-coefficient=polyfit(-A12,E1,1);  %用一次函数拟合曲线，想用几次函数拟合，就把n设成那个数
+coefficient=polyfit(-A12,E1,1); 
 y1=polyval(coefficient,-A12);
 plot(-A12,y1,'k-.')
 xlabel('Atrophy1,2')
