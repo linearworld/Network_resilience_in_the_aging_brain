65e83b92639dafbbfee6d306583805c81aea8af2
linearworld <39855658+linearworld@users.noreply.github.com>
2024-02-05 22:44:06 +0800

    Update newform_callcsec.m

--- empirical_data_code_fig2&3/newform_callcsec.m
+++ empirical_data_code_fig2&3/newform_callcsec.m
@@ -1,7 +1,3 @@
-% 函数2
-% 功能：计算两个序列data1和data2的c序列，c序列就是其中一个时间序列data2错位移动后，和data1计算得到的相关性系数的序列
-% 输入：两个时间序列
-% 输出：c序列
 %  c_s=newform_callcsec(data1, data2);
 
 function  c_s=newform_callcsec(data1,data2)
